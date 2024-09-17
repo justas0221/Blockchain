53909b04e584191dca6076394c60546db5d9e6d8
justas0221 <justaskuz@gmail.com>
2024-09-17 20:27:44 +0300

    The frame of the program

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+std::string hash_function(std::string input);
+
+int main()
+{
+    std::string input, output;
+
+    output = hash_function(input);
+
+    std::cout << output << std::endl;
+
+    return 0;
+}
+
+std::string hash_function(std::string input)
+{
+    std::string output;
+
+    return output;
+}
